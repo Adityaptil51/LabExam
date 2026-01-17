b81c1cfdd26256082ef8d26e8d8e085a5e5670c9
Adityaptil51 <adityaapatil51@gmail.com>
2026-01-17 08:42:57 +0530

    saved

--- app.py
+++ app.py
@@ -1,1 +1,18 @@
-print("Hello welcome to python programming")
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+c = int(input("Enter third number: "))
+
+if a > b and a > c:
+    print("The Number", a, "is gretest")
+elif b > c and b > a:
+    print("The Number", b, "is gretest")
+else:
+    print("The Number", c, "is gretest")
+
+
+if a < b and a < c:
+    print("The Number", a, "is smallest")
+elif b < c and b < a:
+    print("The Number", b, "is smallest")
+else:
+    print("The Number", c, "is smallest")
